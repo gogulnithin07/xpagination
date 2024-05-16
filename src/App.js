59d9254e8288d3b26f484b25f23ee1f877eda45a
martine9696 <gogulnithin07>
2024-05-16 22:16:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [counter, setCounter] = useState(10);
+  let startI = counter === 10 ? 0 : counter - 10;
+  useEffect(function () {
+    async function getData() {
+      try {
+        const req = await fetch(
+          "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+        );
+        const res = await req.json();
+        console.log(res);
+        setData((c) => res);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Employee Data Table</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>name</th>
+              <th>email</th>
+              <th>role</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* {} */}
+            {data.slice(startI, counter).map((obj) => (
+              <CreateTr
+                id={obj.id}
+                name={obj.name}
+                email={obj.email}
+                role={obj.role}
+              />
+            ))}
+            <CreateTr />
+          </tbody>
+        </table>
+        <div className="buttons">
+          <button
+            onClick={(e) => {
+              if (counter === 10) {
+              } else {
+                setCounter((c) => c - 10);
+              }
+            }}>
+            Previous
+          </button>
+          <p>{counter / 10}</p>
+          <button
+            onClick={(e) => {
+              if (counter < data.length) setCounter((c) => c + 10);
+            }}>
+            Next
+          </button>
+        </div>
+      </div>
     </div>
+  );
+}
+function CreateTr({ id, name, email, role }) {
+  return (
+    <>
+      <tr className="row">
+        <td>{id}</td>
+        <td>{name}</td>
+        <td>{email}</td>
+        <td>{role}</td>
+      </tr>
+    </>
   );
 }
 
